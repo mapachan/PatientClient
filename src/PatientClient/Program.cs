78e507281d2a1f983e9a39ef5d0b747939600aca
mapachan <mapachanjie@gmail.com>
2016-07-05 22:32:49 +0200

    Add project files.

---
+++ src/PatientClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PatientClient
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Accessing Patient Database");
+
+            Console.ReadLine();
+
+        }
+    }
+}
